442dd960009a921c85a13ea764636d5595219462
Ian Loam <ialoam126@gmail.com>
2026-02-27 23:24:13 -0800

    Change baseUrl from quartz.jzhao.xyz to ianloam.com

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "ianloam.com",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
